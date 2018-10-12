0f89755c01a09bb6803b0dff317cc52dd7c6a4ca
Orescent <orescent.guthert@myy.haaga-helia.fi>
2018-10-12 14:57:19 +0300

    First part of the web security

--- src/main/java/serv/prog/Bookstore/web/BookController.java
+++ src/main/java/serv/prog/Bookstore/web/BookController.java
@@ -23,6 +23,11 @@ public class BookController {
 	@Autowired
 	private CategoryRepository crepository;
 	
+	@RequestMapping(value="/login")
+    public String login() {	
+        return "login";
+    }
+	
 	@RequestMapping("/index")
 	public String hello(Model model) {
 		return null;
